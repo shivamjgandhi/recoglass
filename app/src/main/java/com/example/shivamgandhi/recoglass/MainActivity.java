806ede161022ea0d33e4ee1abf69757be011b224
sjgandhi1998 <shivam.jgandhi@gmail.com>
2017-02-04 21:02:34 -0500

    Finished saving to the disk

--- app/src/main/java/com/example/shivamgandhi/recoglass/MainActivity.java
+++ app/src/main/java/com/example/shivamgandhi/recoglass/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.shivamgandhi.recoglass;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +23,7 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
     ImageView mImageView;
     Button button1, button2;
     EditText editText;
@@ -40,13 +43,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button2.setOnClickListener(this);
     }
 
-    private void dispatchTakePictureIntent(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
@@ -59,9 +55,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v){
         if(v == findViewById(R.id.Save_Face)){
+            //
             dispatchTakePictureIntent();
-            //Save the picture on disk with the title
-
         }
         else if(v == findViewById(R.id.Search_Face)){
             dispatchTakePictureIntent();
@@ -90,5 +85,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/shivamgandhi/recoglass/RegisterActivity.java
+++ app/src/main/java/com/example/shivamgandhi/recoglass/RegisterActivity.java
@@ -1,4 +1,4 @@
-package com.sourcey.materiallogindemo;
+package com.example.shivamgandhi.recoglass;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
